1869b4cd6e8d1d3c3c34c04af73c331b2ee6c1fe
Cyril Jaquier <cyril.jaquier@jaqpot.net>
2017-07-14 15:43:41 +0200

    Add high state to tank

--- controller/tank.py
+++ controller/tank.py
@@ -12,9 +12,9 @@ logger = logging.getLogger(__name__)
 
 class Tank(PoupoolActor):
 
-    STATE_REFRESH_DELAY = 5
+    STATE_REFRESH_DELAY = 10
 
-    states = ["stop", "low", "normal"]
+    states = ["stop", "low", "normal", "high"]
 
     def __init__(self, encoder, devices):
         super(Tank, self).__init__()
@@ -25,6 +25,7 @@ class Tank(PoupoolActor):
 
         self.__machine.add_transition("low", "*", "low")
         self.__machine.add_transition("normal", "*", "normal")
+        self.__machine.add_transition("high", "*", "high")
         self.__machine.add_transition("stop", "*", "stop")
 
     def on_enter_stop(self):
@@ -38,7 +39,7 @@ class Tank(PoupoolActor):
         self.__encoder.tank_state("low")
         self.__devices.get_valve("main").on()
 
-    @repeat(delay=STATE_REFRESH_DELAY)
+    @repeat(delay=STATE_REFRESH_DELAY / 2)
     def do_repeat_low(self):
         # Security feature: stop if we stay too long in this state
         if self.__machine.get_time_in_state() > datetime.timedelta(hours=1):
@@ -48,6 +49,7 @@ class Tank(PoupoolActor):
                 filtration.stop()
             raise StopRepeatException
         height = self.__devices.get_sensor("tank").value
+        logger.debug("Tank level: %d" % height)
         if height >= 25:
             self._proxy.normal()
             raise StopRepeatException
@@ -71,3 +73,21 @@ class Tank(PoupoolActor):
         if height < 10:
             self._proxy.low()
             raise StopRepeatException
+        elif height >= 75:
+            self._proxy.high()
+            raise StopRepeatException
+
+    @do_repeat()
+    def on_enter_high(self):
+        logger.info("Entering high state")
+        self.__encoder.tank_state("high")
+        self.__devices.get_valve("main").off()
+
+    @repeat(delay=STATE_REFRESH_DELAY * 2)
+    def do_repeat_high(self):
+        height = self.__devices.get_sensor("tank").value
+        logger.debug("Tank level: %d" % height)
+        if height < 60:
+            self._proxy.normal()
+            raise StopRepeatException
+
