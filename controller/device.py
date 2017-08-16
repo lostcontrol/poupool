4de8f6678762dd27b3a63fc14d26dad1712cafdb
Cyril Jaquier <cyril.jaquier@jaqpot.net>
2017-08-16 08:49:02 +0200

    Add 1-wire temperature reading
    Read the sensors and publish the value to the MQTT broker.

--- controller/device.py
+++ controller/device.py
@@ -1,6 +1,7 @@
 import transitions
 import time
 import logging
+import re
 from .util import mapping, constrain
 
 logger = logging.getLogger("device")
@@ -82,6 +83,37 @@ class SensorDevice(Device):
     @property
     def value(self):
         return 50
+
+
+class TempSensorDevice(SensorDevice):
+
+    CRE = re.compile(" t=(\d+)$")
+
+    def __init__(self, name, address):
+        super().__init__(name)
+        self.__address = address
+        self.__path = "/sys/bus/w1/devices/%s/w1_slave" % address
+
+    def __read_temp_raw(self):
+        with open(self.__path, "r") as f:
+            return f.readlines()
+
+    @property
+    def value(self):
+        data = None
+        # Retry up to 3 times
+        for _ in range(3):
+            raw = self.__read_temp_raw()
+            if len(raw) == 2:
+                crc, data = raw
+                if crc.endswith("YES"):
+                    break
+            time.sleep(0.1)
+        else:
+            return None
+        # CRC valid, read the data
+        match = CRE.search(data)
+        return int(match.group(1)) / 1000. if match else None
 
 
 class TankSensorDevice(SensorDevice):
---
+++ controller/temperature.py
@@ -0,0 +1,25 @@
+import logging
+from .actor import PoupoolActor
+from .actor import repeat
+
+logger = logging.getLogger(__name__)
+
+
+class Temperature(PoupoolActor):
+
+    READ_DELAY = 60
+
+    def __init__(self, encoder, sensors):
+        super().__init__()
+        self.__encoder = encoder
+        self.__sensors = sensors
+
+    @repeat(delay=READ_DELAY)
+    def do_read(self):
+        for sensor in self.__sensors:
+            value = sensor.value
+            if value:
+                rounded = round(value, 1)
+                logger.debug("Temperature (%s) is %.1f°C" % (sensor.name, rounded))
+                f = getattr(self.__encoder, sensor.name)
+                f(rounded)
