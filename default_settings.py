b7f5ef7ced2d66ac7df220393f0148b6d2cc2ab9
Cyril Jaquier <cyril.jaquier@jaqpot.net>
2017-08-28 08:05:17 +0200

    Change default tank duration setting
    One hour is enough.

--- default_settings.py
+++ default_settings.py
@@ -30,7 +30,7 @@ def main():
     publish("/settings/filtration/duration", 10 * 3600)
     publish("/settings/filtration/hour_of_reset", 0)
     publish("/settings/filtration/boost_duration", 5 * 60)
-    publish("/settings/filtration/tank_duration", 2 * 3600)
+    publish("/settings/filtration/tank_duration", 1 * 3600)
     publish("/settings/filtration/stir_duration", 5 * 60)
     publish("/settings/filtration/backwash_period", 30)
     publish("/settings/filtration/speed/standby", 0)
